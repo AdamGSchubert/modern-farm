392db0a1e30d9e2dc38123a9bb4b4c9cf9e238cc
Adam Schubert <adamgschubert@gmail.com>
2023-02-27 11:50:37 -0600

    adding seed files

---
+++ src/scripts/seeds/potato.js
@@ -0,0 +1,24 @@
+/*define a function to create seed
+//function should be named like createCorn
+use an arrow function 
+
+each function should return an object with following properties
+    type potato
+    height  32
+    output  2
+
+corn is exception
+    return an array with 2 identical objects each with proper 'keys" and values
+//export function 
+
+
+*/
+
+export let createPotato = () =>{//creates seed object with properties
+    let potatoe ={
+        type: "Potato",
+        height: 32,
+        output: 2
+    }
+    return potatoe
+}
